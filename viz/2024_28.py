a7e0e423970280335cce32a0ca97039e9ab6b66e
dwisdom0 <2480711+dwisdom0@users.noreply.github.com>
2025-10-12 21:45:21 -0700

    save the 2024_28 plot as HTML

--- viz/2024_28.py
+++ viz/2024_28.py
@@ -148,6 +148,10 @@ def main():
     fig.update_xaxes(range=[-pct_clamp, pct_clamp])
     fig.show()
 
+    #fig.write_html("figure_friday_2024_28.html", include_plotlyjs="cdn")
+
+
+
 
 if __name__ == "__main__":
     main()
